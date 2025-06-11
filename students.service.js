f0d1fd683416512b0650b731542b76b20a1eac31
LadisFuentes <albertoladis218@gmail.com>
2025-06-10 20:57:44 -0600

    Add files via upload

--- students.service.js
+++ students.service.js
@@ -1,13 +1,53 @@
- const makeStudentsService=(name,lastname,age)=>{
-    
-    try {
-     return `your name :${name}\n your lastname :${lastname}\n your age :${age}`
+const { createContext } = require("react")
+
+ //Funcion normal
+ const animalService=()=>{}
+
+
+
+//factory function
+const animalService2=()=>({
+   name: "dogi",
+   age:"3 años",
+   color:"blanco y negro",
+
+   create : (name)=> {
+      return`${name}`
+   },
    
-    } catch (error) {
-       return error 
-    }
+
+     update   : ()=> {
+      return"updating animal"
+   },
+   delete : ()=>{
+      return"Deleting animal"
+   },
+   getAll :()=>{
+      return"getting animals"
+   }
+   
+
+
+
+})
+const animalS=animalService()
+   
+console.log(animalS.create);
+console.log(animalS.update);
+console.log(animalS.delete);
+console.log(animalS.getAll);
+
+ class Animal{
+   name
+   contructor(name){
+      this.name=name
+
+   }
+   eat(){
+      return"El animal esta comiendo"
+   }
  }
 
- const makeStudents=makeStudentsService("Ladis", "Fuentes","18")
+ const animal= new Animal("tom")
 
- console.log(makeStudents);
+ console.log(animal.eat());
